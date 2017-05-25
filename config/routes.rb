bf607d0362b0e415d86bb2e4035852bfa1f961f8
David Higgins <david@gryphonandrook.com>
2017-05-25 14:03:33 -0600

    Update Organization routes
    Only use index, show, new, edit, & update actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :organizations
+  resources :organizations, only: [:index, :show, :new, :edit, :update]
   resources :projects, only: [:index, :show, :update, :destroy]
 
   root to: 'organizations#index'
