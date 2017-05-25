c397f1d59886e5dae8d3314bf302306f6713bd81
David Higgins <david@gryphonandrook.com>
2017-05-25 10:32:52 -0600

    Harvest API import for Staff
    Methods to update Staff Data from Harvest API.

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -2,4 +2,24 @@ class Staff < ApplicationRecord
   has_many :entries, dependent: :destroy
   has_many :projects, through: :entries
   belongs_to :organization
+
+  after_create :update_data_from_api
+
+  def self.find_or_create_with(harvest_id: nil, org: nil)
+    raise 'Missing Harvest User Data' if harvest_id.nil? || org.nil?
+    find_or_create_by(harvest_id: harvest_id, organization: org)
+  end
+
+  def update_data_from_api(data = nil)
+    data ||= organization.harvesting.request
+                         .users.find(harvest_id)
+    raise 'Missing Harvest User Data' if data.nil?
+    update(
+      first_name:         data[:first_name],
+      last_name:          data[:last_name],
+      email:              data[:email],
+      harvest_created_at: data[:created_at],
+      harvest_updated_at: data[:updated_at]
+    )
+  end
 end
