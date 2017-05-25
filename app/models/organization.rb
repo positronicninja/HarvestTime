9b7534e3365be71b9924e09781d5d96f6bd58f40
David Higgins <david@gryphonandrook.com>
2017-05-25 10:04:28 -0600

    Harvest API import for Organization
    Methods to update Organization Data & Projects from Harvest API. Only import active Projects by default

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -6,6 +6,20 @@ class Organization < ApplicationRecord
     @harvesting ||= connect_to_api
   end
 
+  def update_via_api
+    account = harvesting.request.account.who_am_i
+    update(name:      account.company.name,
+           base_uri:  account.company.base_uri,
+           active:    account.company.active)
+  end
+
+  def update_projects_from_api(force_update: false)
+    harvesting.request.projects.all.each do |proj_data|
+      break unless force_update || proj_data['active']
+      Project.update_data_for_project(data: proj_data, org: self)
+    end
+  end
+
   private
 
   def connect_to_api
