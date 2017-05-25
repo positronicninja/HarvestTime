4113415b6b9ed3fddc8657f70cb091f844bc83ad
David Higgins <david@gryphonandrook.com>
2017-05-25 15:37:04 -0600

    Collaboration Model
    Proxy object to associate joint Projects

---
+++ app/models/collaboration.rb
@@ -0,0 +1,14 @@
+class Collaboration < ApplicationRecord
+  has_many :projects,       dependent: :destroy
+  has_many :entries,        through: :projects, dependent: :destroy
+  has_many :organizations,  through: :projects
+  has_many :staff, -> { distinct.group 'organization_id' }, through: :entries
+
+  def update_data_from_api(data = nil)
+    projects.each { |proj| proj.update_data_from_api }
+  end
+
+  def update_entries_from_api
+    projects.each { |proj| proj.update_entries_from_api }
+  end
+end
